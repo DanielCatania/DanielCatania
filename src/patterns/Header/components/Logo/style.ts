c6fe9785541691d3c9ec81235ec75d60d1630149
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-07-14 15:23:40 -0300

    adding logo component

---
+++ src/patterns/Header/components/Logo/style.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Logo = styled.svg`
+  @media screen and (min-width: 0px) {
+    width: 80px;
+    height: 80px;
+  }
+  @media screen and (min-width: 744px) {
+    width: 100px;
+    height: 30px;
+  }
+  @media screen and (min-width: 1024px) {
+    width: 120px;
+    height: 35px;
+  }
+`;
+
+export default Logo;
