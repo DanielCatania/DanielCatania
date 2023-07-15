1a3432c31a230e9390636fb9f50b38e2cd64b640
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-07-15 14:19:14 -0300

    adding background gradient animation

--- src/screens/home/components/Presentation/style.ts
+++ src/screens/home/components/Presentation/style.ts
@@ -3,7 +3,6 @@ import styled from "styled-components";
 const Presentation = styled.section`
   display: flex;
   justify-content: center;
-  flex-direction: column;
   align-items: center;
   padding-top: 100px;
 
@@ -11,9 +10,13 @@ const Presentation = styled.section`
 
   &.dark {
     background: ${({ theme }) => theme.colors.gradient["001"]};
+    background-size: 300% 300%;
+    animation: gradient 5s ease infinite;
   }
   &.light {
     background: ${({ theme }) => theme.colors.gradient["002"]};
+    background-size: 300% 300%;
+    animation: gradient 5s ease infinite;
   }
 
   @media screen and (min-width: 0px) {
@@ -22,6 +25,18 @@ const Presentation = styled.section`
 
   @media screen and (min-width: 1024px) {
     height: 100vh;
+  }
+
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
   }
 `;
 
