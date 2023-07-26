c4fc7a1cad5b4921774081aa022a8a2bc5dd58b4
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-07-26 15:08:40 -0300

    fixing section component

--- src/components/Section/index.ts
+++ src/components/Section/index.ts
@@ -2,13 +2,13 @@ import styled from "styled-components";
 
 const Section = styled.section`
   @media screen and (min-width: 0px) {
-    padding: ${({ theme }) => theme.sizes.padding["xs"]};
+    padding: ${({ theme }) => theme.sizes.padding.section["xs"]};
   }
   @media screen and (min-width: 744px) {
-    padding: ${({ theme }) => theme.sizes.padding["md"]};
+    padding: ${({ theme }) => theme.sizes.padding.section["md"]};
   }
   @media screen and (min-width: 1024px) {
-    padding: ${({ theme }) => theme.sizes.padding["lg"]};
+    padding: ${({ theme }) => theme.sizes.padding.section["lg"]};
   }
 `;
 
