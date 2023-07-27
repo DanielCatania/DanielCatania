8957e356df639e89d41889c10afab73c2ef451c8
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-07-27 12:28:03 -0300

    adding skills data

---
+++ src/screens/home/components/About/components/Skills/data.ts
@@ -0,0 +1,54 @@
+const skillsData = [
+  {
+    name: "HTML",
+    alt: "HTML 5",
+    src: "html.png",
+  },
+  {
+    name: "CSS",
+    alt: "CSS 3",
+    src: "css.png",
+  },
+  {
+    name: "JavaScript",
+    alt: "JS",
+    src: "js.png",
+  },
+  {
+    name: "TypeScript",
+    alt: "TS",
+    src: "ts.png",
+  },
+  {
+    name: "ReactJS",
+    alt: "React",
+    src: "react.png",
+  },
+  {
+    name: "NextJS",
+    alt: "Next",
+    src: "next.png",
+  },
+  {
+    name: "Tailwind",
+    alt: "Tailwind",
+    src: "tailwind.png",
+  },
+  {
+    name: "CSS-In-JS",
+    alt: "Styled-Components",
+    src: "styled-components.png",
+  },
+  {
+    name: "Figma",
+    alt: "Figma",
+    src: "figma.png",
+  },
+  {
+    name: "NodeJS",
+    alt: "Node",
+    src: "node.png",
+  },
+];
+
+export default skillsData;
